6386c02498dfe6677727b05e675c94623e9c762a
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-10-04 14:23:25 +0200

    add error screen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
 import { useGetWeather } from "./src/hooks/useGetWeather";
+import ErrorItem from "./src/components/ErrorItem";
 
 export default function App() {
   const { loading, error, weather } = useGetWeather();
@@ -17,7 +18,11 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <ActivityIndicator size={"large"} color={"blue"} />
+      {loading ? (
+        <ActivityIndicator size={"large"} color={"blue"} />
+      ) : (
+        <ErrorItem />
+      )}
     </View>
   );
 }
